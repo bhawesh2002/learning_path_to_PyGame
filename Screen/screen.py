33ab957ea9717d2946fcd2696fe5f4cf89e99f4f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 12:37:46 +0530

    a note on pygame.quit function

--- Screen/screen.py
+++ Screen/screen.py
@@ -11,4 +11,6 @@ window = pygame.display.set_mode((height, width))
 while(pygame.get_init() == True):  # keep showing the screen until the user closes it
     NULL  # do nothing
 
-pygame.quit()  # quit all the initialized modules when python finishes running
+# Modules that are initialized also usually have a quit() function that will clean up.
+# There is no need to explicitly call these,
+# as pygame will cleanly quit all the initialized modules when python finishes.
