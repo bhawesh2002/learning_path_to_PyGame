26f6a4bb13cbddf01e0216b7d8d94352bbf82571
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 23:15:53 +0530

    ways to quit the program

--- color changes/color changes.py
+++ color changes/color changes.py
@@ -12,6 +12,12 @@ window.fill((255, 255, 255))
 
 
 def main():
+    while(True):
+        pygame.display.update()
+        for event in pygame.event.get():
+            if (event.type == QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_q)):
+                pygame.quit()
+                sys.exit()
 
 
 if __name__ == '__main__':
