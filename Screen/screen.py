a8dddcad6f22046cbaa0725f9f64dc64ad807b01
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 12:31:37 +0530

    use pygame.display.set_mode to set the window size

--- Screen/screen.py
+++ Screen/screen.py
@@ -3,3 +3,5 @@ import pygame   #imports the pygame library as module into the program
 pygame.init()   #function call attempts  to initialise all the pygame  modules
 height = 800
 width = 600
+
+window = pygame.display.set_mode((height,width))  #creates a window of the specified size
