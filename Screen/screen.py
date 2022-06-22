dbc40f9384c74c410150990c4e3c2e6eba897825
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 12:36:50 +0530

    use an infinite while loop to keep showing the screen

--- Screen/screen.py
+++ Screen/screen.py
@@ -1,9 +1,14 @@
-import pygame   #imports the pygame library as module into the program
+from asyncio.windows_events import NULL
+import pygame  # imports the pygame library as module into the program
 
-pygame.init()   #function call attempts  to initialise all the pygame  modules
+pygame.init()  # function call attempts  to initialise all the pygame  modules
 height = 800
 width = 600
 
-window = pygame.display.set_mode((height,width))  #creates a window of the specified size
+# creates a window of the specified size
+window = pygame.display.set_mode((height, width))
 
-pygame.quit()   #quit all the initialized modules when python finishes running
+while(pygame.get_init() == True):  # keep showing the screen until the user closes it
+    NULL  # do nothing
+
+pygame.quit()  # quit all the initialized modules when python finishes running
