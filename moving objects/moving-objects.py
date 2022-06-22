a390254b4b7e7efe5bbe370b7f52fb0a0a903777
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 21:41:51 +0530

    call the draw_screen funct

--- moving objects/moving-objects.py
+++ moving objects/moving-objects.py
@@ -10,9 +10,11 @@ height = 800
 width = 600
 window = pygame.display.set_mode((height, width))
 
+
 def draw_screen():
-    window.fill((255,255,255))
+    window.fill((255, 255, 255))
     pygame.display.update()
+
 
 def main():
     while(True):
@@ -21,6 +23,8 @@ def main():
             if (event.type == QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_q)):
                 pygame.quit()
                 sys.exit()
+        draw_screen()
+
 
 if __name__ == '__main__':
     main()
