3aaef5f8fdab70b4d5ead739e8c60bca5f0a76d9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 12:53:15 +0530

    create  an iinfinite while loop

--- Screen/screen.py
+++ Screen/screen.py
@@ -7,6 +7,8 @@ width = 600  # sets the width of the screen
 # creates a window of the specified size
 window = pygame.display.set_mode((height, width))
 
+while(True):   # infinite loop
+
 # Modules that are initialized also usually have a quit() function that will clean up.
 # There is no need to explicitly call these,
 # as pygame will cleanly quit all the initialized modules when python finishes.
