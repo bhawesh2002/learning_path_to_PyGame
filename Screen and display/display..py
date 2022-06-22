747dda860a7651a9346293f09279d2ccb60ff4e3
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 13:04:22 +0530

    create and exit the screen of 800 * 600

--- Screen and display/display..py
+++ Screen and display/display..py
@@ -1,3 +1,13 @@
 import sys # for sys.exit()
 import pygame # for pygame.init()
 from pygame.locals import * #import the locals from the pygame module
+
+height = 800
+width = 600
+
+while(TRUE):
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
