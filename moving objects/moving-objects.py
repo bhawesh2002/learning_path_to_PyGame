9bbafa2945eff91a1d630945bb6a9567b91aacfd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 21:32:05 +0530

    create a window

--- moving objects/moving-objects.py
+++ moving objects/moving-objects.py
@@ -1,4 +1,11 @@
 import sys  # for sys.exit()
-import os # for os.path.isfile()
+import os
+from turtle import width # for os.path.isfile()
 import pygame  # for pygame.init()
 from pygame.locals import *  # import the locals from the pygame module
+
+pygame.init()
+
+height = 800
+width = 600
+window = pygame.display.set_mode((height, width))
