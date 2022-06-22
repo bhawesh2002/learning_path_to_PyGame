d55e23b49347dcf35fce28b9246ae9be232ccff2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 23:29:01 +0530

    allow the ball to move

--- color changes/color changes.py
+++ color changes/color changes.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import sys
 import os
 import pygame
@@ -12,20 +13,43 @@ beach_ball = pygame.image.load(os.path.join(
     "color changes", "Assets", "beach-ball.jpg"))
 
 
-def draw_screen():
+def draw_screen(xpos, ypos, sizex=150, sizey=150):
     window.fill((255, 255, 255))
     beach_ball_resized = pygame.transform.scale(beach_ball, (150, 150))
-    window.blit(beach_ball_resized,(200,200))
+    window.blit(beach_ball_resized, (xpos, ypos))
 
 
 def main():
+    xpos = 100
+    ypos = 100
     while(True):
         pygame.display.update()
         for event in pygame.event.get():
             if (event.type == QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_q)):
                 pygame.quit()
                 sys.exit()
-        draw_screen()
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            if xpos == 0:
+                NULL
+            else:
+                xpos -= 1
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            if xpos == 600:
+                NULL
+            else:
+                xpos += 1
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            if ypos == 500:
+                NULL
+            else:
+                ypos += 1
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            if ypos == 0:
+                NULL
+            else:
+                ypos -= 1
+
+        draw_screen(xpos, ypos)
 
 
 if __name__ == '__main__':
