71826d942760dec4e64f147313e4bde02f101c3a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 16:57:56 +0530

    refactor the code

--- Screen and display/display.py
+++ Screen and display/display.py
@@ -10,7 +10,8 @@ width = 600  # set the width of the screen
 logo = pygame.image.load(
     "Screen and Display\logo\display.png")  # load the logo
 # create a window of the specified size
-window = pygame.display.set_mode((height, width), pygame.RESIZABLE) # set the window size and create a resizeable window
+# set the window size and create a resizeable window
+window = pygame.display.set_mode((height, width), pygame.RESIZABLE)
 pygame.display.set_caption("Display Tweaks")  # set the caption of the window
 pygame.display.set_icon(logo)  # set the icon of the window
 
@@ -22,4 +23,4 @@ while(True):  # infinite game loop
             sys.exit()  # exit the program
         if (event.type == pygame.KEYUP and event.key == pygame.K_f):  # if the user releases the 'f' key
             pygame.display.toggle_fullscreen()  # toggle fullscreen
-    window.fill((65,75,90))  # fill the window with a color
+    window.fill((65, 75, 90))  # fill the window with a color
