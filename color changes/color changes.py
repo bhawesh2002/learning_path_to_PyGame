d34ce923a67b1b81e72ea89f77a002bc1d9b4061
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-22 23:24:09 +0530

    load,resize and blit the  ball on window

--- color changes/color changes.py
+++ color changes/color changes.py
@@ -8,7 +8,14 @@ pygame.init()
 height = 800
 width = 600
 window = pygame.display.set_mode((height, width))
-window.fill((255, 255, 255))
+beach_ball = pygame.image.load(os.path.join(
+    "color changes", "Assets", "beach-ball.jpg"))
+
+
+def draw_screen():
+    window.fill((255, 255, 255))
+    beach_ball_resized = pygame.transform.scale(beach_ball, (150, 150))
+    window.blit(beach_ball_resized,(200,200))
 
 
 def main():
@@ -18,6 +25,7 @@ def main():
             if (event.type == QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_q)):
                 pygame.quit()
                 sys.exit()
+        draw_screen()
 
 
 if __name__ == '__main__':
