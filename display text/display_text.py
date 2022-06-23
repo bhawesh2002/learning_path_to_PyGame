82a59e69592a2d2b26493d5e6efab3a96e4e0f56
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-23 23:12:39 +0530

    document the code

--- display text/display_text.py
+++ display text/display_text.py
@@ -1,30 +1,30 @@
-import sys  
-import pygame
+import sys   # for sys.exit()
+import pygame #import pygame module
 
-from pygame.locals import *
+from pygame.locals import *  #import contains of pygame.locals
 
-pygame.init()
+pygame.init() #initialize pygame
 
-height = 800
-width = 600
+height = 800 #height of the window
+width = 600 #width of the window 
 
-window = pygame.display.set_mode((height, width), pygame.RESIZABLE)
-font = pygame.font.SysFont("comicsansms", 30)
-text = font.render("Hello World", True, (0, 0, 0))
+window = pygame.display.set_mode((height, width), pygame.RESIZABLE) #create a window
+font = pygame.font.SysFont("comicsansms", 30) #create a font object
+text = font.render("Hello World", True, (0, 0, 0)) #create a text object that will be displayed on the screen
 
-def draw_screen():
-    window.fill((255, 255, 255))
-    window.blit(text, (300, 400))
-    pygame.display.update()
+def draw_screen(): #function to draw the screen
+    window.fill((255, 255, 255)) #fill the screen with white color
+    window.blit(text, (300, 400)) #blit the text object to the screen
+    pygame.display.update() #update the screen
 
 
-def main():
+def main(): #main function
     while(True):
         for event in pygame.event.get():
             if (event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q)):
                 pygame.quit()
                 sys.exit()
-        draw_screen()
+        draw_screen() #call the draw_screen function
 
 
 if __name__ == "__main__":
