d9a23dfd985bc517920c0ca2d5995ebd166fca8a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 22:19:22 +0530

    add important lines

--- collision/collision.py
+++ collision/collision.py
@@ -7,3 +7,10 @@ pygame.init()
 position = [1000,600]
 window  = pygame.display.set_mode(position)
 pygame.display.set_caption("Collision")
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
