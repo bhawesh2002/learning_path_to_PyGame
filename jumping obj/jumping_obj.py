cadf2d6d680b10eea0440bae323e004eef9f5133
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:43:51 +0530

    insert soccer ball on the screen

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -9,9 +9,10 @@ height = 600
 width = 800
 
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
-
+socer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
 while(True):
     window.fill((25,205,14))  # fill the screen with a light green color
+    window.blit(socer_ball, (100,100))
     pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
