eb0913e45aed770b721dcf9f2be0b2d223ea2605
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:38:33 +0530

    quit the gamme

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -9,3 +9,10 @@ height = 600
 width = 800
 
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
+
+while(True):
+    pygame.display.update()
+    for event in pygame.event.get():
+        if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
+            pygame.quit()
+            sys.exit()
