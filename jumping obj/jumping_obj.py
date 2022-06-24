9571fd35d20377cb3b78ec482b262bc3919abbcb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:46:33 +0530

    change screen color and set colorkey

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -11,8 +11,9 @@ width = 800
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 soccer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
 soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150,150))
+soccer_ball_scaled.set_colorkey((255,255,255))
 while(True):
-    window.fill((25,205,14))  # fill the screen with a light green color
+    window.fill((255,255,255))  # fill the screen with a light green color
     window.blit(soccer_ball_scaled, (100,100))
     pygame.display.update()
     for event in pygame.event.get():
