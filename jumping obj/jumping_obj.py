b526cdb2496a94cc7343772988534559529284e8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:44:57 +0530

    resize the socer ball

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -9,10 +9,11 @@ height = 600
 width = 800
 
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
-socer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
+soccer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
+soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150,150))
 while(True):
     window.fill((25,205,14))  # fill the screen with a light green color
-    window.blit(socer_ball, (100,100))
+    window.blit(soccer_ball_scaled, (100,100))
     pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
