80eea668139e6494d488c4e7b3d83b442b74b4f7
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:40:25 +0530

    set the screen color to light green

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -11,6 +11,7 @@ width = 800
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 
 while(True):
+    window.fill((25,205,14))  # fill the screen with a light green color
     pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
