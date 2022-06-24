8fb5ec01db368e8b7efcbefba7fb03f5317e786d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 09:57:01 +0530

    code to quit the game

--- Learning coordinate system/coordinate system.py
+++ Learning coordinate system/coordinate system.py
@@ -11,6 +11,12 @@ window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 pygame.display.set_caption("Coordinate System")
 
 def main():
+    pygame.display.update()
+    while(True):
+        for event in pygame.event.get():
+            if (event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q)):
+                pygame.quit()
+                sys.exit
 
 
 
