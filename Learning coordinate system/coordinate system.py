c08d6f9c7c8219827ccdc4d5924225292ca6ef45
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 09:53:51 +0530

    define main function

--- Learning coordinate system/coordinate system.py
+++ Learning coordinate system/coordinate system.py
@@ -9,3 +9,11 @@ width = 800
 
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 pygame.display.set_caption("Coordinate System")
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    main()
