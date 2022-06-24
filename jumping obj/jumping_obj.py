fa8bd6d3ec1a775698248abb1836c737441d60f5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:50:16 +0530

    get ball's coordinates and pass it to blit

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -12,9 +12,12 @@ window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 soccer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
 soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150,150))
 soccer_ball_scaled.set_colorkey((255,255,255))
+soccer_ball_rect = soccer_ball_scaled.get_rect()
+x_dir = soccer_ball_rect.x
+y_dir = soccer_ball_rect.y
 while(True):
     window.fill((255,255,255))  # fill the screen with a light green color
-    window.blit(soccer_ball_scaled, (100,100))
+    window.blit(soccer_ball_scaled, (x_dir,y_dir))
     pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
