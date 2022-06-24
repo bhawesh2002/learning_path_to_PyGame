aebaf669cd5e0c2a0f5c21bf617296761832d16d
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 12:56:17 +0530

    set the game's fps to 60

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -7,7 +7,7 @@ pygame.init()
 
 height = 600
 width = 800
-
+clock = pygame.time.Clock()
 window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
 soccer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
 soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150,150))
@@ -16,6 +16,7 @@ soccer_ball_rect = soccer_ball_scaled.get_rect()
 x_dir = soccer_ball_rect.x
 y_dir = soccer_ball_rect.y
 while(True):
+    clock.tick(60)
     window.fill((255,255,255))  # fill the screen with a light green color
     window.blit(soccer_ball_scaled, (x_dir,y_dir))
     pygame.display.update()
@@ -23,3 +24,5 @@ while(True):
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
             sys.exit()
+        if(pygame.key.get_pressed()[pygame.K_RIGHT]):
+            x_dir += 10
