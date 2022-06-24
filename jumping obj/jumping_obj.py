200209f2198b5caeab76fa3584e6d1eb6a303ce5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-24 13:01:11 +0530

    some changes

--- jumping obj/jumping_obj.py
+++ jumping obj/jumping_obj.py
@@ -8,22 +8,29 @@ pygame.init()
 height = 600
 width = 800
 clock = pygame.time.Clock()
-window = pygame.display.set_mode((width,height), pygame.RESIZABLE)
-soccer_ball = pygame.image.load(os.path.join('jumping obj','Assets','soccer-ball.jpg')).convert()
-soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150,150))
-soccer_ball_scaled.set_colorkey((255,255,255))
+window = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+soccer_ball = pygame.image.load(os.path.join(
+    'jumping obj', 'Assets', 'soccer-ball.jpg')).convert()
+soccer_ball_scaled = pygame.transform.scale(soccer_ball, (150, 150))
+soccer_ball_scaled.set_colorkey((255, 255, 255))
 soccer_ball_rect = soccer_ball_scaled.get_rect()
 x_dir = soccer_ball_rect.x
 y_dir = soccer_ball_rect.y
+
+
+def draw_screen():
+    window.fill((255, 255, 255))  # fill the screen with a light green color
+    window.blit(soccer_ball_scaled, (x_dir, y_dir))
+    pygame.display.update()
+    pygame.display.flip()
+
+
 while(True):
     clock.tick(60)
-    window.fill((255,255,255))  # fill the screen with a light green color
-    window.blit(soccer_ball_scaled, (x_dir,y_dir))
-    pygame.display.update()
+
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
             sys.exit()
         if(pygame.key.get_pressed()[pygame.K_RIGHT]):
             x_dir += 10
-    pygame.display.flip()
