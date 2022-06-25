e6ce89409b48bb4604d7c0866feb5ff1a0ff4d10
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-25 14:14:27 +0530

    make the object move

--- collision/collision.py
+++ collision/collision.py
@@ -9,15 +9,24 @@ position = [1000,600]
 window  = pygame.display.set_mode(position)
 pygame.display.set_caption("Collision")
 mouse = pygame.image.load(os.path.join('collision','Assets','mouse.png'))
+mouse_scaled = pygame.transform.scale(mouse,(150,75))
 def main():
-    window.fill((255,255,255))
-    pygame.display.update()
+    x_pos ,y_pos = 0,510
     while True:
+        window.fill((255,255,255))
+        window.blit(mouse_scaled,(x_pos,y_pos))
+        pygame.display.update()
         for event in pygame.event.get():
             if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
                 pygame.quit()
                 sys.exit()
-
-
+        if (pygame.key.get_pressed()[pygame.K_LEFT]):
+            x_pos -= 1
+            if(x_pos < 0):
+                x_pos = 0
+        if (pygame.key.get_pressed()[pygame.K_RIGHT]):
+            x_pos += 1
+            if(x_pos > 1000):
+                x_pos = 1000
 if __name__ == "__main__":
     main()
