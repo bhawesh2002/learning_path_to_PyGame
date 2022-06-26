d168f1b9585070adbec6c1f6654962089aee1edc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-26 23:47:33 +0530

    some changes

--- Jump on Collision/jump_on_collision.py
+++ Jump on Collision/jump_on_collision.py
@@ -10,12 +10,12 @@ height, width = 600, 800
 window = pygame.display.set_mode((width, height))
 mouse = pygame.image.load(os.path.join('Jump on Collision', 'Assets', 'mouse.png')).convert()
 mouse_scaled = pygame.transform.scale(mouse,(150,100))
+x_pos, y_pos = 0,490
 while True:
-    x_pos, y_pos = 0,490
-    pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
             sys.exit()
     window.fill((255,255,255))
     window.blit(mouse_scaled,(x_pos,y_pos))
+    pygame.display.update()
