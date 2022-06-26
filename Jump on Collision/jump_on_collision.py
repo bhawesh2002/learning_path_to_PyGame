4c1e21aa7e66f50a93605dfaa8e4663c7b8d3404
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-26 23:31:50 +0530

    close the window by pressing q

--- Jump on Collision/jump_on_collision.py
+++ Jump on Collision/jump_on_collision.py
@@ -5,6 +5,13 @@ from pygame.locals import *
 
 pygame.init()
 
-height,width = 600,800
+height, width = 600, 800
 
-window = pygame.display.set_mode(width,height)
+window = pygame.display.set_mode(width, height)
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
+            pygame.quit()
+            sys.exit()
