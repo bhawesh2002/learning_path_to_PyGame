0ccfd64c938f1df444e932cce35f0ca231dd66bb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-26 23:42:59 +0530

    create x and y pos

--- Jump on Collision/jump_on_collision.py
+++ Jump on Collision/jump_on_collision.py
@@ -11,10 +11,11 @@ window = pygame.display.set_mode((width, height))
 mouse = pygame.image.load(os.path.join('Jump on Collision', 'Assets', 'mouse.png')).convert()
 mouse_scaled = pygame.transform.scale(mouse,(150,100))
 while True:
+    x_pos, y_pos = 0,490
     pygame.display.update()
     for event in pygame.event.get():
         if (event.type == QUIT or (event.type == KEYDOWN and event.key == K_q)):
             pygame.quit()
             sys.exit()
     window.fill((255,255,255))
-    window.blit(mouse_scaled,(0,490))
+    window.blit(mouse_scaled,(x_pos,y_pos))
