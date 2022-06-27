581050ee3b689ea76e7bf0296e6b1861cf13fc4a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-27 23:10:21 +0530

    draw a ball on the screen

--- bouncy_walls/bouncy_walls.py
+++ bouncy_walls/bouncy_walls.py
@@ -10,6 +10,8 @@ width,height = 800,600
 window = pygame.display.set_mode((width,height))
 
 while True:
+    window.fill((255,255,255))
+    ball = pygame.draw.circle(window,(22,255,22),(55,55),50)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
