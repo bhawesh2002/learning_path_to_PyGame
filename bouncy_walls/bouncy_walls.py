f84a5814589b04183a161cbcaff6a8673f5a06cd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-27 23:30:04 +0530

    coment the code

--- bouncy_walls/bouncy_walls.py
+++ bouncy_walls/bouncy_walls.py
@@ -3,14 +3,14 @@ import os  #for os.path.join()
 import pygame #import the pygame module
 from pygame.locals import *  #import the pygame.locals for easier access to key presses and mouse positions
 
-pygame.init()
+pygame.init()  #initialize the pygame module
 
-width,height = 800,600
+width,height = 800,600  #set the width and height of the screen
 
-window = pygame.display.set_mode((width,height))
-dimensions = window.get_rect()
-x_pos = 400
-y_pos = 0
+window = pygame.display.set_mode((width,height))  #create the window
+dimensions = window.get_rect() #get the dimensions of the window
+x_pos = 400 #set the x position of the ball
+y_pos = 0 #set the y position of the ball
 while True:
     window.fill((255,255,255))
     ball = pygame.draw.circle(window,(22,255,22),(x_pos,y_pos),25)
