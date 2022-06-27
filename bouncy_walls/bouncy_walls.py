90f634243eb768a82cb5efdc0fee3a1a62b6c380
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-27 23:17:04 +0530

    get the dimensions of the window

--- bouncy_walls/bouncy_walls.py
+++ bouncy_walls/bouncy_walls.py
@@ -8,7 +8,8 @@ pygame.init()
 width,height = 800,600
 
 window = pygame.display.set_mode((width,height))
-
+dimensions = window.get_rect()
+pos = dimensions.midtop
 while True:
     window.fill((255,255,255))
     ball = pygame.draw.circle(window,(22,255,22),(55,55),25)
