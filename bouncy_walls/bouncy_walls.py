10e1fbb0dea8538b0dbe17db876e67ea8ad86b62
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-27 23:27:52 +0530

    move the ball towards right

--- bouncy_walls/bouncy_walls.py
+++ bouncy_walls/bouncy_walls.py
@@ -1,7 +1,7 @@
-import sys
-import os
-import pygame
-from pygame.locals import *
+import sys #for sys.exit()
+import os  #for os.path.join()
+import pygame #import the pygame module
+from pygame.locals import *  #import the pygame.locals for easier access to key presses and mouse positions
 
 pygame.init()
 
@@ -9,12 +9,18 @@ width,height = 800,600
 
 window = pygame.display.set_mode((width,height))
 dimensions = window.get_rect()
-pos = dimensions.midtop
+x_pos = 400
+y_pos = 0
 while True:
     window.fill((255,255,255))
-    ball = pygame.draw.circle(window,(22,255,22),(pos),25)
+    ball = pygame.draw.circle(window,(22,255,22),(x_pos,y_pos),25)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             pygame.quit()
             sys.exit()
+    if(pygame.key.get_pressed()[K_RIGHT]):
+        if(x_pos < dimensions.width):
+            x_pos += 1
+        else:
+            x_pos = dimensions.width
