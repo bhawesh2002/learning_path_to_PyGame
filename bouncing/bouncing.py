d0c91503138e6dab79478cf6ad6d3f293725d175
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-29 01:49:42 +0530

    code

--- bouncing/bouncing.py
+++ bouncing/bouncing.py
@@ -10,21 +10,24 @@ window = pygame.display.set_mode((width, height))
 dimensions = window.get_rect()
 x_pos = dimensions.centerx
 y_pos = dimensions.centery
+counter = 3
 while True:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             pygame.quit()
             sys.exit()
     if(y_pos < dimensions.bottomleft[1] - 25):
-        while(y_pos != dimensions.bottomleft[1] - 25):
+        while(y_pos <= dimensions.bottomleft[1] - 25):
             y_pos += 0.5
             window.fill((0, 0, 0))
             obj = pygame.draw.circle(window, (22, 225, 22), (x_pos, y_pos), 25)
             pygame.display.update()
-        if(y_pos == dimensions.bottomleft[1] - 25):
-            while(y_pos != dimensions.bottomleft[1] - 200):
-                y_pos -= 0.5
+        if(y_pos >= dimensions.bottomleft[1] - 25):
+            counter -= 1
+            while(y_pos >= dimensions.bottomleft[1] - 200):
+                y_pos -= 0.3
                 window.fill((0, 0, 0))
-                obj = pygame.draw.circle(window, (22, 225, 22), (x_pos, y_pos), 25)
+                obj = pygame.draw.circle(
+                    window, (22, 225, 22), (x_pos, y_pos), 25)
                 pygame.display.update()
     pygame.display.update()
