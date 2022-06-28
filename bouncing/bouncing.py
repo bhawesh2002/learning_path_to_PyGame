1ca9619dc3b14db391d0a4a6ac93d57053a287d8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-28 23:00:49 +0530

    make the ball bounce

---
+++ bouncing/bouncing.py
@@ -0,0 +1,30 @@
+import sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+width, height = 800, 600
+
+window = pygame.display.set_mode((width, height))
+dimensions = window.get_rect()
+x_pos = dimensions.centerx
+y_pos = dimensions.centery
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
+            pygame.quit()
+            sys.exit()
+    if(y_pos < dimensions.bottomleft[1] - 25):
+        while(y_pos != dimensions.bottomleft[1] - 25):
+            y_pos += 0.5
+            window.fill((0, 0, 0))
+            obj = pygame.draw.circle(window, (22, 225, 22), (x_pos, y_pos), 25)
+            pygame.display.update()
+        if(y_pos == dimensions.bottomleft[1] - 25):
+            while(y_pos != dimensions.bottomleft[1] - 200):
+                y_pos -= 0.5
+                window.fill((0, 0, 0))
+                obj = pygame.draw.circle(window, (22, 225, 22), (x_pos, y_pos), 25)
+                pygame.display.update()
+    pygame.display.update()
