8c9cc8b9e6573093c72fdbc0e118eef87d24d725
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-28 17:33:51 +0530

    draw a ball and make it move towRDS RIGHT

--- trace the path/trace_path.py
+++ trace the path/trace_path.py
@@ -1,3 +1,4 @@
+from cmath import e
 import sys
 import pygame
 from pygame.locals import *
@@ -12,8 +13,14 @@ y_pos = dimensions.centery
 radius = 25
 while True:
     window.fill((0,0,0))
+    ball = pygame.draw.circle(window,(22,255,22),(x_pos,y_pos),radius)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             pygame.quit()
             sys.exit()
+    if(pygame.key.get_pressed()[K_RIGHT]):
+        if(x_pos < dimensions.topright[0]):
+            x_pos += 1
+        else:
+            x_pos = dimensions.topright[0]
