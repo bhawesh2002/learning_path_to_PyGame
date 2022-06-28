85fca2c510adc1fa4584a525d59d320ddc54c769
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-28 17:01:14 +0530

    quit the window

--- trace the path/trace_path.py
+++ trace the path/trace_path.py
@@ -4,6 +4,12 @@ from pygame.locals import *
 
 pygame.init()
 
-width,height = 800*600
+width,height = 800,600
 window = pygame.display.set_mode((width,height))
 dimensions = window.get_rect()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN or pygame.key == K_q):
+            pygame.quit()
+            sys.exit()
