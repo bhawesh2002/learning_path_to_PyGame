226e537d566f9b3274d26cce522f2e2163cb7219
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-29 10:08:16 +0530

    add gravity to the object

--- bouncing/bouncing.py
+++ bouncing/bouncing.py
@@ -11,9 +11,15 @@ dimensions = window.get_rect()
 x_pos = dimensions.centerx
 y_pos = dimensions.centery
 counter = 3
+gravity = 0.5
 while True:
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             pygame.quit()
             sys.exit()
+    if(y_pos <= dimensions.bottomright[1] - 25):
+        y_pos += gravity
+        window.fill((0,0,0))
+        ball = pygame.draw.circle(window,(22,255,22),(x_pos,y_pos),25)
+        pygame.display.update()
     pygame.display.update()
