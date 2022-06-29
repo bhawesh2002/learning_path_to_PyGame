74a5f455722e0dfcf0b427198b211c8d4ffd3ac4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-29 10:18:13 +0530

    wrap in main function

--- bouncing/bouncing.py
+++ bouncing/bouncing.py
@@ -8,18 +8,29 @@ width, height = 800, 600
 
 window = pygame.display.set_mode((width, height))
 dimensions = window.get_rect()
-x_pos = dimensions.centerx
-y_pos = dimensions.centery
-counter = 3
 gravity = 0.5
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
-            pygame.quit()
-            sys.exit()
-    if(y_pos <= dimensions.bottomright[1] - 25):
-        y_pos += gravity
-        window.fill((0,0,0))
-        ball = pygame.draw.circle(window,(22,255,22),(x_pos,y_pos),25)
+
+
+def main():
+    x_pos = dimensions.centerx
+    y_pos = dimensions.centery
+    counter = 3
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
+                pygame.quit()
+                sys.exit()
+        if(y_pos <= dimensions.bottomright[1] - 25):
+            y_pos += gravity
+            window.fill((0, 0, 0))
+            ball = pygame.draw.circle(
+                window, (22, 255, 22), (x_pos, y_pos), 25)
+            pygame.display.update()
+            if(y_pos >= dimensions.bottomright[1] - 25):
+                counter -= 1
+                print(counter)
         pygame.display.update()
-    pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
