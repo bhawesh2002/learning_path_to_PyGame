82056f9f4091b5d876057f43f0625ac51672ddbc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-29 10:40:54 +0530

    create a resist variable

--- bouncing/bouncing.py
+++ bouncing/bouncing.py
@@ -15,6 +15,7 @@ def main():
     x_pos = dimensions.centerx
     y_pos = dimensions.centery
     counter = 3
+    resist = 50
     while True:
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
@@ -28,9 +29,10 @@ def main():
             pygame.display.update()
             if(y_pos >= dimensions.bottomright[1] - 25):
                 counter -= 1
-                while(y_pos >= dimensions.centery):
+                while(y_pos >= (dimensions.centery + resist)):
                     y_pos -= 0.1
-                    window.fill((0,0,0))
+                    resist += 0.1
+                    window.fill((0, 0, 0))
                     ball = pygame.draw.circle(
                         window, (22, 255, 22), (x_pos, y_pos), 25)
                     pygame.display.update()
