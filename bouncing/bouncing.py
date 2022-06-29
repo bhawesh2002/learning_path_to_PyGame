de86d1d995ff067105ec1aaef9c2ce4e4d5b2b3a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-29 09:57:57 +0530

    start from beggning

--- bouncing/bouncing.py
+++ bouncing/bouncing.py
@@ -16,18 +16,4 @@ while True:
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_q):
             pygame.quit()
             sys.exit()
-    if(y_pos < dimensions.bottomleft[1] - 25):
-        while(y_pos <= dimensions.bottomleft[1] - 25):
-            y_pos += 0.5
-            window.fill((0, 0, 0))
-            obj = pygame.draw.circle(window, (22, 225, 22), (x_pos, y_pos), 25)
-            pygame.display.update()
-        if(y_pos >= dimensions.bottomleft[1] - 25):
-            counter -= 1
-            while(y_pos >= dimensions.bottomleft[1] - 200):
-                y_pos -= 0.3
-                window.fill((0, 0, 0))
-                obj = pygame.draw.circle(
-                    window, (22, 225, 22), (x_pos, y_pos), 25)
-                pygame.display.update()
     pygame.display.update()
